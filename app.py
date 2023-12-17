208daad9ea5382f9b361b5d6529901612013a4c5
1Akashm <magarakash183@gmail.com>
2023-12-17 13:43:03 +0545

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import pickle
+import numpy as np
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
@@ -6,9 +8,11 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/predict')
+@app.route('/predict', methods=['POST','GET'])
 def predict():
     return render_template('predict.html')
+    features = [float(x) for x in request.form.values()]
+    final= [np.array(features)]
 
 @app.route('/data')
 def data():
